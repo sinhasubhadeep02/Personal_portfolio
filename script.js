d15e75af9e4633c532cb7047765eb7b06c494e3f
Subhadeep Sinha <sinha.subhadeep02@gmail.com>
2023-07-29 13:24:26 +0530

    adding new things

--- script.js
+++ script.js
@@ -71,3 +71,13 @@ $(document).ready(function(){
         }
     });
 });
+
+// Function to open the popup
+function openPopup() {
+    document.getElementById('popupContainer').style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById('popupContainer').style.display = 'none';
+}
